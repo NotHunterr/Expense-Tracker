47450e368cf442b27380702a0dc428fff7b4436b
NotHunterr <notryoshi@gmail.com>
2023-11-28 14:32:56 -0800

    Revert "I'm working on this I swear"
    This reverts commit 6ef66d437085a6842edca9e98ae2ed6e1249005d.

--- index.js
+++ index.js
@@ -5,8 +5,3 @@ formQuestions.addEventListener('submit', (event) => {
   let monthlyIncome = document.getElementById('monthly_income').value
   console.log(monthlyIncome)
 })
-
-let showMonthly = () => {
-  let monthlyInput = document.getElementById('show_monthly')
-  monthlyInput.style.display = 'block'
-}
