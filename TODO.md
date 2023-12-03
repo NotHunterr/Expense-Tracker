2118c6e424b2762a59fbcfb495b2336211a61d40
Hunter <notryoshi@gmail.com>
2023-12-03 12:20:18 -0800

    Working on being able to dynamically add debts

---
+++ TODO.md
@@ -0,0 +1,7 @@
+TODO
+
+# Procedurally add more debts, no max amount.
+
+# -- within this, ask for minimum monthly payment
+
+# ADD Dropdown Menu for what type of debt it is
--- index.js
+++ index.js
@@ -2,12 +2,21 @@ let formQuestions = document.getElementById('form_questions')
 let monthlyDiv = document.querySelector('.monthly_div')
 let yearlyDiv = document.querySelector('.yearly_div')
 
-formQuestions.addEventListener('submit', (event) => {
-  event.preventDefault()
-  let monthlyIncome = document.getElementById('monthly_income').value
+formQuestions.addEventListener('submit', (e) => {
+  e.preventDefault()
+  let monthlyIncome = document.getElementById('show_monthly_income').value
   console.log(monthlyIncome)
 })
 
+yearlyDiv.addEventListener('submit', (e) => {
+  e.preventDefault()
+  let yearlyIncome = document.getElementById('show_yearly_income').value
+  console.log(yearlyIncome)
+  monthlyIncome = yearlyIncome / 12
+})
+
+/* Showing and hiding the showing/monthly income option when opposite
+ button is pressed. */
 let showMonthly = () => {
   monthlyDiv.style.display = 'block'
   yearlyDiv.style.display = 'none'
@@ -16,4 +25,15 @@ let showMonthly = () => {
 let showYearly = () => {
   monthlyDiv.style.display = 'none'
   yearlyDiv.style.display = 'block'
+}
+
+const numOfDebts = document.querySelector('.debt-questions')
+const getNewDebts = document.querySelector('.num-of-debts')
+let debtArr = []
+
+const fnAddDebt = () => {
+  const debtContainer = document.createElement('div')
+  debtContainer.classList.add('debt-container')
+  addText = document.querySelector('.debt-container')
+  addText.innerHTML += 'Hello'
 }
