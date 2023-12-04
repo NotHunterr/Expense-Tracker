9f88dfcf20335b691f0d96a6e51e7bb1d0aaaa6c
Hunter <notryoshi@gmail.com>
2023-12-03 16:44:08 -0800

    User can now enter their debts.

--- index.js
+++ index.js
@@ -28,12 +28,64 @@ let showYearly = () => {
 }
 
 const numOfDebts = document.querySelector('.debt-questions')
-const getNewDebts = document.querySelector('.num-of-debts')
-let debtArr = []
+const getNewDebts = document.querySelector('.num-of-debts-container')
 
-const fnAddDebt = () => {
-  const debtContainer = document.createElement('div')
-  debtContainer.classList.add('debt-container')
-  addText = document.querySelector('.debt-container')
-  addText.innerHTML += 'Hello'
-}
+/* STEPS TO ADDING THIS: 
+    CREATE A NEW DIV
+    ADD DESIRED TEXT TO DIV
+    ALLOW USER TO ADD THEIR DEBTS
+      BE ABLE TO TRACK THE DEBT THAT THEY ADDED
+      STORE MMP, INTEREST, AMOUNT AS AN OBJECT IN ARRAY
+      BE ABLE TO ADD THE INPUTS INTO THE OBJECT
+      THEN PARSE THROUGH THE DATA TO FIND MOST/LEAST IMPORTANT!!! :D
+    ALLOW USER TO DELETE A DEBT.  
+*/
+
+// const fnAddDebt = () => {
+//   const newDiv = document.createElement('div')
+//   newDiv.appendChild(document.createTextNode('Hello'))
+//   numOfDebts.innerHTML += 'Hello'
+// }
+
+debtSubmit = document.getElementById('submit-btn')
+let debtObj = []
+let debtNum = 1
+
+debtSubmit.addEventListener('click', (e) => {
+  e.preventDefault()
+  let debtAmountValue = document.getElementById('debt-amount').value
+  let interestAmountValue = document.getElementById('interest-amount').value
+  let mmpAmountValue = document.getElementById('min-monthly-payment').value
+  /////// Need to do this because I want to reset input fields :/
+  let debtAmountInput = document.getElementById('debt-amount')
+  let interestAmountInput = document.getElementById('interest-amount')
+  let mmpAmountInput = document.getElementById('min-monthly-payment')
+
+  // Creating a new object that stores the debt values submitted
+  let newDebtObject = {
+    debt: debtAmountValue,
+    interest: interestAmountValue,
+    mmp: mmpAmountValue,
+  }
+
+  // Pushing the newly created object into a string
+  debtObj.push(newDebtObject)
+  console.log(debtObj)
+
+  if (debtNum == 1) {
+    alert('You have inserted a debt. If you have more, please keep inputting.')
+    debtNum++
+  } else {
+    alert(
+      `You have inserted ${debtNum} total debts. If you have more, please keep inputting.`
+    )
+    debtNum++
+  }
+
+  // Resetting input fields for a second debt.
+  debtAmountInput.value = ''
+  interestAmountInput.value = ''
+  mmpAmountInput.value = ''
+
+  //Display current objects as a test!
+})
